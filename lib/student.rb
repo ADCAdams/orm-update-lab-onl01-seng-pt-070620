488d9b0d0f52bc8aabe953efaacc2bfd46d5bbd8
Andy Adams <andrewdcadams@gmail.com>
2020-09-27 21:55:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,10 +62,7 @@ class Student
     WHERE name = ?
     SQL
     result = DB[:conn].execute(sql,name_var)
-    puts result
     found_student = Student.new(result[0], result[1], result[2])
-    puts found_student
-    puts found_student.name
   end
 
 end
